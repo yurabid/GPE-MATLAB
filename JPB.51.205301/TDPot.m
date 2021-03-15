67804c48a220680821ad15391f2a12039acb0337
Yuriy Bidasyuk <yurabid@gmail.com>
2021-03-15 21:55:33 +0100

    further cleanup

--- JPB.51.205301/TDPot.m
+++
@@ -1,8 +0,0 @@
-% Time-dependent part of the potential
-function VVV = TDPot(XXg,~,time)
-
-global Ub vv rscale;
-
-VVV = Ub*exp(-2*((XXg - vv*time)./(3.5e-6/rscale)).^2); % Gaussian barrier with amplitude Ub and 1/e^2 half width 3.5 \mu m
-
-end
